096eb9afe64a8482c5c38b0b9d3280631c741eb2
Matt O'Tousa <mlotousa@gmail.com>
2016-09-22 16:05:43 -0500

    hi res main image

--- mysite.js
+++ mysite.js
@@ -1,23 +1,12 @@
-var likedThings = ['art', 'music', 'dogs', 'space', 'nature', 'leg day', 'kind people', 'water', 'swimming', 'bass', 'ukulele', 'bikes', 'stars', 'painting', 'the number 4', 'turtles', 'elephants', 'penguins', 'orangutans', 'books', 'mountains', 'buffalo wings', 'the internet', 'computers', 'javascript', 'quantum physics', 'beer', 'steak', 'open-source', 'wasting time', 'pianos', 'polyrhythms', 'DSP', 'chill people', "chillin'", 'the oxford comma', 'hardcover books', 'painting', 'the moon', 'the stars', 'the clouds', 'the ocean', 'snowboarding', 'secret beatboxing', 'kangaroos', 'dunkaroos', 'my fam', 'quakkas', 'tomatoes', 'peaches', 'quesadillas', 'oscilloscopes', 'signal processing', 'machine learning', 'dreaming', 'yawning', 'subwoofers', 'sick beats', 'puns', 'brick jokes', 'apples', 'oranges', 'hiking', 'eating', 'pizza', 'science', 'memes', 'grass', 'rain', 'smooth rocks', 'sleeping', 'blue skies', 'showers', 'old fashioneds', 'whiskey', 'coral reefs', 'Earth', 'games', 'puzzles', 'fiction', 'sci-fi', 'fantasy', 'video games', 'storms', 'programming', 'drawing', 'designing', 'filming', 'recording', 'editing', 'lifting', "hangin' out", 'theremins', 'trains', 'neural networks', 'stories', 'Mars', 'NASA', 'Jupiter', 'painting', 'snacking', 'wubba-lubba-dub-dub', 'books', 'ravioli', 'spaghetti', 'porkchops', 'cheese', 'ice cream', 'the beach', 'the universe', 'the milky way', 'our solar system', 'parks', 'national parks' ];
+var likedThings = ['art', 'music', 'dogs', 'space', 'nature', 'leg day', 'water', 'swimming', 'bass', 'ukulele', 'bikes', 'stars', 'painting', 'the number 4', 'turtles', 'elephants', 'penguins', 'orangutans', 'books', 'mountains', 'buffalo wings', 'the internet', 'computers', 'javascript', 'quantum physics', 'beer', 'steak', 'open-source', 'wasting time', 'pianos', 'polyrhythms', 'DSP', 'chill people', "chillin'", 'the oxford comma', 'hardcover books', 'painting', 'the moon', 'the stars', 'the clouds', 'the ocean', 'snowboarding', 'secret beatboxing', 'kangaroos', 'dunkaroos', 'my fam', 'quakkas', 'tomatoes', 'peaches', 'quesadillas', 'oscilloscopes', 'signal processing', 'machine learning', 'dreaming', 'yawning', 'subwoofers', 'sick beats', 'puns', 'brick jokes', 'apples', 'oranges', 'hiking', 'eating', 'pizza', 'science', 'memes', 'grass', 'rain', 'smooth rocks', 'sleeping', 'blue skies', 'showers', 'old fashioneds', 'whiskey', 'coral reefs', 'Earth', 'games', 'puzzles', 'fiction', 'sci-fi', 'fantasy', 'video games', 'storms', 'programming', 'drawing', 'designing', 'filming', 'recording', 'editing', 'lifting', "hangin' out", 'theremins', 'trains', 'neural networks', 'stories', 'Mars', 'NASA', 'Jupiter', 'painting', 'snacking', 'wubba-lubba-dub-dub', 'books', 'ravioli', 'spaghetti', 'porkchops', 'cheese', 'ice cream', 'the beach', 'the universe', 'the milky way', 'our solar system', 'parks', 'national parks' ];
 
 var interestDelay = 1600;
 var whichInterest = 4;
 
 $( document ).ready(function() {
-	//here we get window height and adjust front page padding...
-	var imgHeight = 360;
-	var toFill = $(window).height() - (98 + imgHeight + 68 + 120 + 80);
-	console.log (toFill);
-	if ( toFill > 1 ) {
-		var space = toFill/2;
-		$( '#top-spacer' ).css('height', space + 'px');
-		console.log( $('#top-spacer').height() );
-		$( '#bottom-spacer' ).css('height', space + 'px');
-	} else {
-		$('#top-spacer').css('height', 100 + 'px');
-	};
 
-
+	//sizes up front page real nice
+	frontPagePadder();
 	//randomly changes my interests at interestDelay interval, man
 	setTimeout(onTimeOut, interestDelay)
 
@@ -46,6 +35,26 @@ $( document ).ready(function() {
 
 		//}
 	setTimeout(onTimeOut, interestDelay);
+	}
+
+	function frontPagePadder() {
+		//here we get window height and adjust front page padding...
+		var imgHeight = 360;
+		var toFill = $(window).height() - (98 + imgHeight + 68 + 120 + 80);
+		console.log (toFill);
+		if ( toFill > 1 ) {
+			var space = toFill/8; //divide by 8 to do 5/8 on top, 3/8 on bottom
+			$( '#top-spacer' ).css('height', space*5 + 'px');
+			console.log( $('#top-spacer').height() );
+			$( '#bottom-spacer' ).css('height', space*3 + 'px');
+		} else {
+			$('#top-spacer').css('height', 100 + 'px');
+		};
+
+		//position main image inside div #main-img
+		$( '#main-img' ).css('background-position-y', -250 + 'px')
+
+		console.log ('made it!');
 	}
 
 });
